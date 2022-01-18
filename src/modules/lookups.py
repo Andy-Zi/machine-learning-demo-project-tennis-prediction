5cd88beef6c678c80e08ef964257ffb4e50b21a4
LLukas22 <65088241+LLukas22@users.noreply.github.com>
2022-01-18 13:27:06 +0100

    player_match_history_lookup hinzufegügt

---
+++ src/modules/lookups.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:26b279756cf59327fa4b7bdb203a05809c5a94dcbcb6d9d2a2fc68ece7a53bfb
+size 7597
---
+++ src/modules/scoreparser.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f4e3c0d2a931643d3064b6f1e43333552396071d7ac54cfa9d4057647698050e
+size 3309
